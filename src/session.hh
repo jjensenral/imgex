eff1122cab3bbfff54f7fd9216a4c12b77499528
jens jensen <j.jensen.ral@outlook.com>
2022-04-30 19:25:59 +0100

    fix: cmake build; bug in session.hh init

--- src/session.hh
+++ src/session.hh
@@ -9,23 +9,23 @@ typedef std::chrono::duration<int64_t> ses_time;
 
 class Session final {
  private:
-	uint64_t id_;
-	std::chrono::time_point<ses_time> time_;
+    uint64_t id_;
+    std::chrono::time_point<std::chrono::system_clock> time_;
 
  public:
-	Session();
-	~Session();
+    Session();
+    ~Session();
 
-	/** Persist all session data into file
-	 * \param filename - location to write session (filename, directory).
-	 * location defaults to current working directory if writeable, and /tmp if not */
-	void persist(char const *filename = nullptr);
+    /** Persist all session data into file
+     * \param filename - location to write session (filename, directory).
+     * location defaults to current working directory if writeable, and /tmp if not */
+    void persist(char const *filename = nullptr);
 
-	/** Lock session - all images - from mousing etc */
-	void lock();
+    /** Lock session - all images - from mousing etc */
+    void lock();
 
-	/** Unlock, opening all images to manipulation */
-	void unlock();
+    /** Unlock, opening all images to manipulation */
+    void unlock();
 };
 
 
